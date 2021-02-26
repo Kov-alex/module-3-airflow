1b62b193ea8ac49f5270d505051ed8205aa2ca60
Kov-alex <79699865+Kov-alex@users.noreply.github.com>
2021-02-26 22:52:09 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -14,19 +14,23 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
+rockets = ['all', 'falcon1', 'falcon9', 'falconheavy']
+
 dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
-t1 = BashOperator(
-    task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
-    dag=dag
-)
+for rocket in rockets:
 
-t2 = BashOperator(
-    task_id="print_data", 
-    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-    params={"rocket": "all"}, # falcon1/falcon9/falconheavy
-    dag=dag
-)
+    t1 = BashOperator(
+        task_id="get_data", 
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r {{ rocket }}", 
+        dag=dag
+    )
 
-t1 >> t2
+    t2 = BashOperator(
+        task_id="print_data", 
+        bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
+        params={"rocket": {{ rocket }}}, # falcon1/falcon9/falconheavy
+        dag=dag
+    )
+
+    t1 >> t2
